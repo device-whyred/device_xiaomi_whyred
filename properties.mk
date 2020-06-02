b502c9a1eab16fc661f5fe21b9d788ce784e7aa3
erfanoabdi <erfangplus@gmail.com>
2020-06-02 12:00:49 +0600

    wayne: Retrive time info from persist * This allows time to be correct on first boot
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- properties.mk
+++ properties.mk
@@ -14,7 +14,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.imx376_ofilm.light.lux=275 \
     persist.vendor.imx376_ofilm.low.lux=290 \
     persist.vendor.imx376_sunny.light.lux=275 \
-    persist.vendor.imx376_sunny.low.lux=290
+    persist.vendor.imx376_sunny.low.lux=290 \
+    persist.vendor.delta_time.enable=true
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
