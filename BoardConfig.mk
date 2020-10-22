35d50f6088401fd10ffb0204568043e851538bf8
SakilMondal <mondalsakil@gmail.com>
2020-10-22 15:11:40 +0530

    whyred: Update Vendor Security Patch from jasmine V11.0.14.0.QDIMIXM
    - Updated all blobs from jasmine V11.0.14.0.QDIMIXM so why not.
    
    Change-Id: I0597825d974cbc30b17711103104fcb9da631d0f
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-06-01
+VENDOR_SECURITY_PATCH := 2020-09-05
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_whyred
