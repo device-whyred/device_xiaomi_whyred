ae88d4737f2799b43a011ab4c7d865831df5a9ae
Mohammad Iqbal <mi460334@gmail.com>
2020-10-29 11:02:45 +0000

    add maintaner strings

--- colt_whyred.mk
+++ colt_whyred.mk
@@ -21,7 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Inherit some common Colt stuff
 $(call inherit-product, vendor/colt/config/common_full_phone.mk)
+
+#Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080 x 2160
+
+# Maintainer
+COLT_DEVICE_MAINTAINER := Mohammad iqbal
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
